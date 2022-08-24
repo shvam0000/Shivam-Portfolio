58362be78733a4a16f401bf9c1480fd8346ce77c
shvam0000 <shvam0000@gmail.com>
2022-08-24 13:43:56 -0300

    :sparkles: experience and projects

--- src/portfolio.js
+++ src/portfolio.js
@@ -320,108 +320,143 @@ const experience = {
   title: "Experience",
   subtitle: "Work, Internship and Volunteership",
   description:
-    "I've completed one internship. I've mostly done projects on my own and I am actively looking for internships. I love organizing workshops to share my knowledge with others.",
+    "I've completed various internships in Software Engineering and Designing. I've mostly been a self learner and showcase my learning through my projects. I love organizing workshops and events to share my experience with others.",
   header_image_path: "experience.svg",
   sections: [
     {
       title: "Work Experience",
       experiences: [
         {
-          title: "Full Stack Developer (Intern)",
-          company: "Valora Infotech",
-          company_url: "https://valora-infotech.business.site/",
-          logo_path: "valora.jpg",
-          duration: "Oct 2020 - Mar 2021",
-          location: "Office",
+          title: "HRI Research Intern",
+          company: "MITACS",
+          company_url: "https://www.mitacs.ca/",
+          logo_path: "mitacs-logo.png",
+          duration: "June 2022 - Sept 2022",
+          location: "University of New Brunswick, New Brunswick, Canada",
           description: `Train, manage and provide guidance to junior software development staff.
           Work closely with the Project Manager and Team Leads on change request functions.
           Develops new and maintains existing applications.
           Increased productivity and problem-solving technics by 20%.
           We primarily used PERN and MERN Stack. 80% of my work was doing backend development and the remaining 20% was DevOps-related tasks.
           `,
-          // "I worked on the Dashboard project which helps users track their activities while using Walo Application. I also worked on Ocean Inventory Application and it's Admin panel Backend as well as on Ocean Inventory Admin Front-end using React and also worked on Walo Admin Backend.",
           color: "#0071C5",
         },
         {
-          title: "Full Stack Developer (Freelancer)",
-          company: "NightOwls",
-          company_url: "http://nightowls.company/",
-          logo_path: "nightowls.jpg",
-          duration: "Sep 2020 - Oct 2020",
-          location: "Work From Home",
+          title: "Software Developer Intern",
+          company: "Quinence",
+          company_url: "http://quinence.com/",
+          logo_path: "quinence-logo.jpg",
+          duration: "Feb 2022 - May 2022",
+          location: "Remote - Singapore",
           description: `Work closely with the Client and Team on change request functions.
           We Primarily used LAMP Stack. 40% of my work was doing backend development, 30% of my work was designing database schemas, and 30% of my work was on maintain hosting and user traffics on the website.
           `,
-          // "Created Front end of Yearn Financial Mutual Funds website. also degined simple web application for better user experience, designed DB Schemas as well.",
+          color: "#ee3c26",
+        },
+        {
+          title: "Software Developer Intern",
+          company: "BITCS",
+          company_url: "http://bitcs.in/",
+          logo_path: "bitcs-logo.png",
+          duration: "Nov 2021 - Feb 2022",
+          location: "Remote - Noida",
+          description: `Work closely with the Client and Team on change request functions.
+          We Primarily used LAMP Stack. 40% of my work was doing backend development, 30% of my work was designing database schemas, and 30% of my work was on maintain hosting and user traffics on the website.
+          `,
+          color: "#ee3c26",
+        },
+        {
+          title: "UX Engineer Intern",
+          company: "Techdome Solutions Pvt. Ltd.",
+          company_url: "https://techdome.io/#",
+          logo_path: "techdome-logo.png",
+          duration: "June 2021 - Sept 2021",
+          location: "Remote - Indore",
+          description: `Work closely with the Client and Team on change request functions.
+          We Primarily used LAMP Stack. 40% of my work was doing backend development, 30% of my work was designing database schemas, and 30% of my work was on maintain hosting and user traffics on the website.
+          `,
           color: "#ee3c26",
         },
       ],
     },
     {
-      title: "Volunteerships",
+      title: "Clubs Experience and Volunteerships",
       experiences: [
         {
-          title: "Cross Winter of Code Mentor",
-          company: "CWOC",
-          company_url: "https://crosswoc.ieeedtu.in/",
-          logo_path: "cwoc.png",
-          duration: "Feb 2021 - Present",
-          location: "Work From Home",
+          title: "Founder - Admin",
+          company: "GitHub Community SRM",
+          company_url: "https://githubsrm.tech/",
+          logo_path: "gcsrm-logo.jpg",
+          duration: "May 2021 - May 2022",
+          location: "SRMIST - Chennai",
           description:
             "Mentorship responsibilities were to help students plan the project, review issues and pull requests, ensure smooth progress and help them out when they are stuck.",
           color: "#4285F4",
         },
         {
-          title: "Campus Hustler",
-          company: "Skillenza",
-          company_url: "https://skillenza.com/",
-          logo_path: "skillenza.png",
-          duration: "Feb 2021 - Present",
-          location: "Work from Home",
+          title: "Member",
+          company: "Next Tech Lab",
+          company_url:
+            "https://www.linkedin.com/company/next-tech-lab/mycompany/",
+          logo_path: "ntl-logo.jpg",
+          duration: "April 2021 - Present",
+          location: "SRMIST - Chennai",
           description:
             "Spread Awareness of new Technologies and new Opportunities to Students and Grow Skillenza Community.",
           color: "#196acf",
         },
         {
-          title: "GitHub Student Developer",
-          company: "GitHub",
+          title: "Mentor",
+          company: "GirlScript Winter of Contributing",
           company_url: "https://github.com/",
-          logo_path: "github.png",
-          duration: "Nov 2019 - Present",
+          logo_path: "gscwoc-logo.jpeg",
+          duration: "Aug 2021 - Dec 2021",
           location: "Work from Home",
           description:
             "Contribute to Open Source Community and Open Source Project.",
           color: "#040f26",
         },
         {
-          title: "Google Local Guide",
-          company: "Google Map",
-          company_url: "https://maps.google.com/localguides/",
-          logo_path: "localguide.png",
-          duration: "Sep 2018 - Present",
-          location: "Work From Home",
+          title: "Developer",
+          company: "IoT Alliance",
+          company_url: "https://www.iotalliance.in/",
+          logo_path: "iota-logo.png",
+          duration: "Nov 2020 - Aug 2021",
+          location: "SRMIST - Chennai",
           description:
             "Day-to-day responsibilities of helping local businesses to spread their business to the world. Helping users by writing reviews about different locations and spaces such as shops, malls, etc.",
           color: "#D83B01",
         },
         {
-          title: "GDG Student Volunteer",
-          company: "Google Developer Groups",
-          company_url: "https://gdg.community.dev/",
-          logo_path: "gdg.png",
-          duration: "Feb 2021 - Present",
+          title: "Web Developer",
+          company: "Guvi Code Camp SRM",
+          company_url:
+            "https://www.linkedin.com/company/guvi-code-camp-srm/?originalSubdomain=in",
+          logo_path: "gcccsrm-logo.jpeg",
+          duration: "Nov 2020 - Apr 2021",
           location: "Work From Home",
           description:
             "Google Developer Group Surat Student Volunteer and Member.",
           color: "#D83B01",
         },
         {
-          title: "E. F. I. Student Volunteer",
-          company: "ENVIRONMENTALIST FOUNDATION OF INDIA",
-          company_url: "https://indiaenvironment.org/",
-          logo_path: "efi.png",
-          duration: "Apr 2017 - Present",
-          location: "Work From Home",
+          title: "Public Relations Associate",
+          company: "Directorate Of Student Affairs , SRMIST",
+          company_url: "https://www.instagram.com/srmist_dsa/?hl=en",
+          logo_path: "dsa-logo.png",
+          duration: "Feb 2020 - Present",
+          location: "SRMIST",
+          description:
+            "Take time out over weekends to join a gang of nation builders, nature enthusiasts to conserve what is left.",
+          color: "#5a900f",
+        },
+        {
+          title: "Quality Assurance Associate",
+          company: "Aaruush , SRMIST",
+          company_url: "https://www.linkedin.com/company/aaruush-srm-ist/",
+          logo_path: "aaruush-logo.jpeg",
+          duration: "Sep 2019 - Sep 2020",
+          location: "SRMIST",
           description:
             "Take time out over weekends to join a gang of nation builders, nature enthusiasts to conserve what is left.",
           color: "#5a900f",
@@ -451,7 +486,7 @@ const contactPageData = {
     title: "Blogs",
     subtitle:
       "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Twitter.",
-    link: "https://twitter.com/Harikrushn9",
+    link: "https://twitter.com/shivam1910200",
     avatar_image_path: "blogs_image.svg",
   },
 };
@@ -460,34 +495,21 @@ const projects = {
   data: [
     {
       id: "0",
-      name: "AutomateInstaPyBot",
-      url: "https://github.com/harikanani/AutomateInstaPyBot",
+      name: "Karaoke Music App",
+      url: "https://github.com/NVombat/MusicApp",
       description:
-        "This is Instagram Bot. This will login to your Instagram account. Follow Users, Unfollow Users, Remove Profile Photo",
+        "This is a Karaoke Music App built for JTV Foundation where users can record audio/video and upload them.",
       languages: [
         {
-          name: "Python",
-          iconifyClass: "logos-python",
-        },
-      ],
-    },
-    {
-      id: "1",
-      name: "react-twitter-clone",
-      url: "https://github.com/harikanani/react-twitter-clone",
-      description:
-        "A React Twitter Clone UI with basic functionality such as make a Tweet.Embedded Profile Tweets and Share on Twitter.",
-      languages: [
-        {
-          name: "HTML5",
-          iconifyClass: "vscode-icons:file-type-html",
+          name: "TypeScript",
+          iconifyClass: "logos-typescript",
         },
         {
-          name: "CSS3",
-          iconifyClass: "vscode-icons:file-type-css",
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
         },
         {
-          name: "React",
+          name: "ReactJS",
           iconifyClass: "logos-react",
         },
         {
@@ -497,15 +519,36 @@ const projects = {
       ],
     },
     {
-      id: "2",
-      name: "node-blockchain",
-      url: "https://github.com/harikanani/node-blockchain",
+      id: "1",
+      name: "Projects Portal - SRM",
+      url: "https://github.com/SRM-IST-KTR/githubsrm",
       description:
-        "A simple blockchain and cryptocurrency wallet implemented in Node.js and TypeScript (for learning purposes).",
+        "A centralized projects portal built for SRMIST to combine all the minor and major projects of the university",
       languages: [
         {
-          name: "NodeJS",
-          iconifyClass: "logos-nodejs",
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "NextJS",
+          iconifyClass: "logos-nextjs",
+        },
+        {
+          name: "TypeScript",
+          iconifyClass: "logos-typescript",
+        },
+      ],
+    },
+    {
+      id: "2",
+      name: "Crypto Tools",
+      url: "https://github.com/shvam0000/crypto-tools",
+      description:
+        "A simple website to include all the cryptography tools such as ciphers etc. The app is built in remix-run framework built on top of ReactJS",
+      languages: [
+        {
+          name: "Remix-run",
+          iconifyClass: "logos-remix",
         },
         {
           name: "TypeScript",
@@ -515,22 +558,13 @@ const projects = {
     },
     {
       id: "3",
-      name: "top-crypto-gainers",
-      url: "https://github.com/harikanani/top-crypto-gainers",
-      description:
-        "A top high price changed crypto coins wring 24 hoursA sound-classifier webapp made with ReactJS + TensorflowJS.",
+      name: "Image Search",
+      url: "https://github.com/shvam0000/Image-Search",
+      description: "An image search app built on ReactJS using Pixbay API.",
       languages: [
         {
-          name: "HTML5",
-          iconifyClass: "vscode-icons:file-type-html",
-        },
-        {
-          name: "CSS3",
-          iconifyClass: "vscode-icons:file-type-css",
-        },
-        {
-          name: "JavaScript",
-          iconifyClass: "logos-javascript",
+          name: "TailwindCSS",
+          iconifyClass: "logos-tailwindcss",
         },
         {
           name: "ReactJS",
@@ -540,10 +574,9 @@ const projects = {
     },
     {
       id: "4",
-      name: "personal-portfolio",
-      url: "https://github.com/harikanani/personal-portfolio",
-      description:
-        "A simple command line interface based quiz app to know more about me :).",
+      name: "Shivam Portfolio",
+      url: "https://github.com/shvam0000/Shivam-Portfolio",
+      description: "A Personal Portfolio built using ReactJS",
       languages: [
         {
           name: "JavaScript",
@@ -552,219 +585,6 @@ const projects = {
         {
           name: "NodeJS",
           iconifyClass: "logos-nodejs",
-        },
-      ],
-    },
-    {
-      id: "3",
-      name: "node_express_crud_api_starter",
-      url: "https://github.com/harikanani/node_express_crud_api_starter",
-      description: "Simple NodeJS Express CRUD Operations API starter.",
-      languages: [
-        {
-          name: "NodeJS",
-          iconifyClass: "logos-nodejs",
-        },
-      ],
-    },
-    {
-      id: "4",
-      name: "node-web-scrapper",
-      url: "https://github.com/harikanani/node-web-scrapper",
-      description:
-        "A Simple web scrapper that scrap the information of amazon products such as price.It also scrap Wikipedia Data such as birthdate.",
-      languages: [
-        {
-          name: "HTML5",
-          iconifyClass: "vscode-icons:file-type-html",
-        },
-        {
-          name: "CSS3",
-          iconifyClass: "vscode-icons:file-type-css",
-        },
-        {
-          name: "NodeJs",
-          iconifyClass: "logos-nodejs",
-        },
-      ],
-    },
-    {
-      id: "5",
-      name: "harikanani.github.io",
-      url: "https://github.com/harikanani/harikanani.github.io",
-      description:
-        "A Personal Portfolio Website that showcases my work and experience. which is hosted on Github Pages.",
-      languages: [
-        {
-          name: "HTML5",
-          iconifyClass: "vscode-icons:file-type-html",
-        },
-        {
-          name: "CSS3",
-          iconifyClass: "vscode-icons:file-type-css",
-        },
-        {
-          name: "JavaScript",
-          iconifyClass: "logos-javascript",
-        },
-        {
-          name: "SCSS",
-          iconifyClass: "vscode-icons:file-type-scss2",
-        },
-      ],
-    },
-    {
-      id: "6",
-      name: "Automate Attendace",
-      url: "https://github.com/harikanani/Node_Python",
-      description: "Automation of Online Attendance using node js and python",
-      languages: [
-        {
-          name: "JavaScript",
-          iconifyClass: "logos-javascript",
-        },
-        {
-          name: "Python",
-          iconifyClass: "logos-python",
-        },
-      ],
-    },
-    {
-      id: "7",
-      name: "Automate Discord Bot",
-      url: "https://github.com/harikanani/AutomateDiscordBot",
-      description:
-        "A Discord Bot to send Automatic messages to serer channel in regular time difference.",
-      languages: [
-        {
-          name: "Python",
-          iconifyClass: "logos-python",
-        },
-        {
-          name: "Python Selenium",
-          iconifyClass: "logos-selenium",
-        },
-        {
-          name: "Chromium Browser",
-          iconifyClass: "openmoji-chromium",
-        },
-      ],
-    },
-    {
-      id: "8",
-      name: "Flask Blog",
-      url: "https://github.com/harikanani/flask_blog",
-      description: "A Simple Blog Web Application made using Flask Framework",
-      languages: [
-        {
-          name: "Python",
-          iconifyClass: "logos-python",
-        },
-        {
-          name: "Flask",
-          iconifyClass: "cib-flask",
-        },
-        {
-          name: "HTML5",
-          iconifyClass: "vscode-icons:file-type-html",
-        },
-        {
-          name: "CSS3",
-          iconifyClass: "vscode-icons:file-type-css",
-        },
-      ],
-    },
-    {
-      id: "9",
-      name: "Netflix top series",
-      url: "https://github.com/harikanani/netflix-top-series",
-      description: "List of Top Netflix Series which is deployed to vercel.",
-      languages: [
-        {
-          name: "JavaScript",
-          iconifyClass: "logos-javascript",
-        },
-        {
-          name: "ReactJS",
-          iconifyClass: "logos-react",
-        },
-        {
-          name: "HTML5",
-          iconifyClass: "logos-html-5",
-        },
-        {
-          name: "CSS3",
-          iconifyClass: "logos-css-3",
-        },
-      ],
-    },
-    {
-      id: "10",
-      name: "COVID-19 Tracker",
-      url: "https://github.com/harikanani/Covid19TrackerReact",
-      description:
-        "Simple Covid-19 Tracker made using React and deployed to Vercel.",
-      languages: [
-        {
-          name: "JavaScript",
-          iconifyClass: "logos-javascript",
-        },
-        {
-          name: "ReactJS",
-          iconifyClass: "logos-react",
-        },
-        {
-          name: "HTML5",
-          iconifyClass: "logos-html-5",
-        },
-        {
-          name: "CSS3",
-          iconifyClass: "logos-css-3",
-        },
-      ],
-    },
-    {
-      id: "11",
-      name: "Food Order Static Website",
-      url: "https://github.com/harikanani/food-order-website",
-      description:
-        "A simple static website related to food restaurants service. this is reasponsive as well.",
-      languages: [
-        {
-          name: "HTML5",
-          iconifyClass: "logos-html-5",
-        },
-        {
-          name: "CSS3",
-          iconifyClass: "logos-css-3",
-        },
-        {
-          name: "JavaScript",
-          iconifyClass: "logos-javascript",
-        },
-        {
-          name: "PHP",
-          iconifyClass: "logos-php",
-        },
-      ],
-    },
-    {
-      id: "12",
-      name: "NFT Launchpad",
-      url: "https://deliquescent-cents.000webhostapp.com/",
-      description: "NFT Launchpad crypto site portfolio",
-      languages: [
-        {
-          name: "HTML5",
-          iconifyClass: "logos-html-5",
-        },
-        {
-          name: "CSS3",
-          iconifyClass: "logos-css-3",
-        },
-        {
-          name: "JavaScript",
-          iconifyClass: "logos-javascript",
         },
         {
           name: "ReactJS",
